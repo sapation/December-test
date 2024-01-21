01a0b296843dc2eace2afd6989ff71e52f410273
iddrisu <sumaila129@gmail.com>
2024-01-21 22:48:02 +0000

    Load font in layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View,FlatList} from 'react-native';
 import DayListItem from '../../src/components/core/DayListItem';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
-import { AmaticSC_400Regular,AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+
 
 
 const days = [...Array(24)].map((val, index)=> index + 1);
 
-SplashScreen.preventAutoHideAsync();
-
 export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold
-  });
-
-  useEffect(()=> {
-     if (fontsLoaded || fontError) {
-        SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError])
- 
-   if (!fontsLoaded && !fontError) {
-        return null;
-    }
 
   return (
     <View style={styles.container}>
